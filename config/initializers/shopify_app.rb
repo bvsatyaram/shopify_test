80bbf8c65d993f9581cbd2e3c165acb682e7e313
BV Satyaram <bvsatyaram@gmail.com>
2017-06-15 14:46:43 +0530

    Update shopify app name and scope

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "Shop The Look"
   config.api_key = "dcd0a100581c1346243ed8d3120500fa"
   config.secret = "585af7a5bdd0d146fc7168218e2c0c62"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_products"
   config.embedded_app = true
 end
